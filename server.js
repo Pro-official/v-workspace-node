68d5c5d9b5c82819e96e0e8676b95d690d955795
Pro-official <promise.iam00@gmail.com>
2022-08-27 00:15:54 +0600

    Initial Backend

---
+++ server.js
@@ -0,0 +1,148 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+const DBUser = process.env.DBUser;
+const DBPassword = process.env.DBPassword;
+const uri = `mongodb+srv://${DBUser}:${DBPassword}@cluster0.qmcox.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("vclass");
+    const usersCollection = database.collection("users");
+    const classesCollection = database.collection("classes");
+    const projectsCollection = database.collection("projects");
+
+    // POST USERS
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.json(result);
+      console.log(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const cursor = usersCollection.find();
+      const users = await cursor.toArray();
+      res.json(users);
+    });
+
+    // UPDATE USERS
+    app.put("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const updatedProfile = req.body;
+      console.log(updatedProfile);
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { type: updatedProfile.type },
+      };
+      const options = { upsert: true };
+      const result = await usersCollection.updateMany(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+      console.log(result, "check if it works");
+    });
+
+    // POST CLASSROOM
+    app.post("/classes", async (req, res) => {
+      const user = req.body;
+      const result = await classesCollection.insertOne(user);
+      res.json(result);
+      // console.log(result);
+    });
+
+    // GET CLASSROOM
+    app.get("/classes", async (req, res) => {
+      const cursor = classesCollection.find();
+      const classes = await cursor.toArray();
+      res.json(classes);
+    });
+
+    // GET CLASS WITH CODE
+    app.get("/classes/:code", async (req, res) => {
+      const code = req.params.code;
+      const filter = { code: code };
+      const cl = await classesCollection.findOne(filter);
+      res.json(cl);
+    });
+
+    // INSERT CLASS INFO IN THE USERS
+    app.put("/users/join/:email", async (req, res) => {
+      const email = req.params.email;
+      const joinedClass = req.body;
+      const filter = { email: email };
+      const updateDoc = {
+        $push: {
+          classInfo: { joinedClass },
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+      console.log(result);
+    });
+
+    // POST PROJECTS
+    app.post("/projects", async (req, res) => {
+      const user = req.body;
+      const result = await projectsCollection.insertOne(user);
+      res.json(result);
+      // console.log(result);
+    });
+
+    // GET PROJECTS
+    app.get("/projects", async (req, res) => {
+      const cursor = projectsCollection.find();
+      const projects = await cursor.toArray();
+      res.json(projects);
+    });
+
+    // GET PROJECT BY ID
+    app.get("/projects/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await projectsCollection.findOne(query);
+      res.json(result);
+    });
+
+    // INSERT POSTS FRON PROJECTS CLASS
+    app.put("/projects/:id", async (req, res) => {
+      const id = req.params.id;
+      const post = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $push: { post },
+      };
+      const result = await projectsCollection.updateOne(filter, updateDoc);
+      res.json(result);
+      console.log(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Virtual Class");
+});
+
+app.listen(port, () => {
+  console.log(`listening at ${port}`);
+});
